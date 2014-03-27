ec56df34c1d246de4d3865eb253a61d30304b720
Josh <notjoshmiller@gmail.com>
2014-03-27 16:01:19 -0500

    Convert to bulk insert. Comments.

--- index.js
+++ index.js
@@ -1,28 +1,36 @@
 /*jshint esnext: true */
 var _s = require('underscore.string');
-var moment = require('moment');
 var co = require('co');
-var mssql = require('co-mssql');
+var fs = require('fs');
+var moment = require('moment');
 var monk = require('monk');
+var mssql = require('co-mssql');
 var wrap = require('co-monk');
 
+/* Initialize MongoDB connection */
 var db = monk('localhost/test');
+
+/* Set up chats collection */
 var chats = wrap(db.get('chats'));
 chats.ensureIndex( { chatID: 1 }, { unique: true } );
 chats.remove();
+
+/* Set up transcripts collection */
 var transcripts = wrap(db.get('transcripts'));
 transcripts.ensureIndex( { messageID: 1 }, { unique: true } );
 transcripts.remove();
 
-var fs = require('fs');
+/* Load SQL into memory */
 var chatSql = fs.readFileSync('chats.sql', 'utf-8');
 var tranSql = fs.readFileSync('transcripts.sql', 'utf-8');
 
+/* Grab date so we can pull chats per day */
 var day = moment().format("DD");
 var month = moment().format("MM");
 var year = moment().format("YYYY");
 
 co(function* () {
+  /* Initialize MSSQL connection */
   var connection = new mssql.Connection({
     user: process.env.DW_USER,
     password: process.env.DW_PASS,
@@ -35,37 +43,51 @@ co(function* () {
     yield connection.connect();
 
     var request = new mssql.Request(connection);
+    /* Inject date into MSSQL query */
     request.input('day', mssql.VarChar, day);
     request.input('month', mssql.VarChar, month);
     request.input('year', mssql.VarChar, year);
+
     var recordset = yield request.query(chatSql);
     var results = [];
     var transArr = [];
+    /* Insert each MSSQL row into chats collection
+     * Push inserted document into results array
+     * Chats that already exist in Mongo will not be inserted and
+     * thus will not be added to the MSSQL query for transcripts */
     for (var record in recordset) {
-      try { 
+      try {
         results.push(yield chats.insert(recordset[record]));
         process.stdout.write("Inserting chat " + recordset[record].chatID + "\r");
+        /* If a chat already exists in Mongo
+        *  then we catch the duplicate key error here */
       } catch (ex) { console.dir(ex.err); }
     }
-    
+
     console.log("\nInserted " + results.length + " chats.");
 
     var requestTrans = new mssql.Request(connection);
+    /* For each inserted chat document query for its transcripts */
     for (var chat in results) {
       process.stdout.write("Pulling transcripts for " + results[chat].chatID + "\r");
       requestTrans.input('chatID', mssql.VarChar, results[chat].chatID);
       var tranSet = yield requestTrans.query(tranSql);
+      /* Iterate over returned transcript rows for each MSSQL query */
       for (var tran in tranSet) {
+        /* Strip HTML from transcript text */
         var text = tranSet[tran].text;
         text = _s.unescapeHTML(text);
         text = _s.stripTags(text);
         tranSet[tran].text = text;
-        try { transArr.push(yield transcripts.insert(tranSet[tran])); }
-        catch (ex) { console.dir(ex); }
+        /* Push each transcript into array for Mongo bulk insert */
+        transArr.push(tranSet[tran]);
       }
     }
 
-    console.log("\nInserted " + transArr.length + " chat messages.");
+    /* Bulk insert transcripts */
+    try { yield transcripts.insert(transArr); }
+    catch (ex) { console.dir(ex); }
+    console.log("\nBulk inserted " + transArr.length + " chat messages.");
 
   } catch (ex) { console.dir(ex); } finally {
     connection.close();
