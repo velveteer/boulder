4afcfb7deac6fa7950df129032b4693b4148c4c7
Josh <notjoshmiller@gmail.com>
2014-03-27 09:20:10 -0500

    Replacing MongoDB with RethinkDB.

--- index.js
+++ index.js
@@ -1,28 +1,31 @@
-/*jshint esnext: true */
-var _s = require('underscore.string');
-var moment = require('moment');
+/*jshint esnext: true*/
 var co = require('co');
 var mssql = require('co-mssql');
-var monk = require('monk');
-var wrap = require('co-monk');
-
-var db = monk('localhost/test');
-var chats = wrap(db.get('chats'));
-chats.ensureIndex( { chatID: 1 }, { unique: true } );
-chats.remove();
-var transcripts = wrap(db.get('transcripts'));
-transcripts.ensureIndex( { messageID: 1 }, { unique: true } );
-transcripts.remove();
-
+var r = require('co-rethinkdb');
 var fs = require('fs');
+var moment = require('moment');
+var _s = require('underscore.string');
+
+/* Connect to Rethink and remove need for run() */
+var conn;
+r.getConnection = function* () {
+  return conn || (conn = yield r.connect({ host: 'localhost', port: 28015}));
+};
+
+/* Read SQL files into memory */
 var chatSql = fs.readFileSync('chats.sql', 'utf-8');
 var tranSql = fs.readFileSync('transcripts.sql', 'utf-8');
 
+/* Set current date so we can pull chats daily */
 var day = moment().format("DD");
 var month = moment().format("MM");
 var year = moment().format("YYYY");
 
 co(function* () {
+
+/* Set up databases */
+  yield r.table('chats').delete();
+  yield r.table('transcripts').delete();
   var connection = new mssql.Connection({
     user: process.env.DW_USER,
     password: process.env.DW_PASS,
@@ -32,18 +35,23 @@ co(function* () {
 
   try {
 
+    /* Connect to MSSQL */
     yield connection.connect();
 
     var request = new mssql.Request(connection);
+    /* Inject today's date into MSSQL query */
     request.input('day', mssql.VarChar, day);
     request.input('month', mssql.VarChar, month);
     request.input('year', mssql.VarChar, year);
+    /* Execute query and return set of records */
     var recordset = yield request.query(chatSql);
     var results = [];
     var transArr = [];
+    /* Iterate over records, insert them into Rethink
+       Then push the insert results into an array (returnVals: true) */
     for (var record in recordset) {
       try { 
-        results.push(yield chats.insert(recordset[record]));
+        results.push(yield r.table('chats').insert(recordset[record],{returnVals: true}));
         process.stdout.write("Inserting chat " + recordset[record].chatID + "\r");
       } catch (ex) { console.dir(ex.err); }
     }
@@ -51,24 +59,32 @@ co(function* () {
     console.log("\nInserted " + results.length + " chats.");
 
     var requestTrans = new mssql.Request(connection);
-    for (var chat in results) {
-      process.stdout.write("Pulling transcripts for " + results[chat].chatID + "\r");
-      requestTrans.input('chatID', mssql.VarChar, results[chat].chatID);
+    /* Iterate over Rethink results array
+       Execute MSSQL query to pull transcripts for each result chatID */
+    for (var i in results) {
+      var chatID = results[i].new_val.chatID;
+      process.stdout.write("Pulling transcripts for " + chatID + "\r");
+      requestTrans.input('chatID', mssql.VarChar, chatID);
       var tranSet = yield requestTrans.query(tranSql);
+     /* Push each MSSQL row returned into an array
+        Strip HTML from transcript text */
       for (var tran in tranSet) {
         var text = tranSet[tran].text;
         text = _s.unescapeHTML(text);
         text = _s.stripTags(text);
         tranSet[tran].text = text;
-        try { transArr.push(yield transcripts.insert(tranSet[tran])); }
-        catch (ex) { console.dir(ex); }
+        transArr.push(tranSet[tran]);
       }
     }
+    console.log("\nBulk inserting transcripts.");
+    try { yield r.table('transcripts').insert(transArr); }
+    catch (ex) { console.dir(ex); }
 
-    console.log("\nInserted " + transArr.length + " chat messages.");
+    console.log("Inserted " + transArr.length + " chat messages.");
 
-  } catch (ex) { console.dir(ex); } finally {
+  } catch (ex) { console.dir(ex); 
+  } finally {
     connection.close();
-    db.close();
+    conn.close();
   }
 })();
