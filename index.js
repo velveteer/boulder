d2229a495c22bf0fa72ce66b8ddd3b23d42f7cee
Josh <notjoshmiller@gmail.com>
2014-05-18 22:47:21 -0500

    Add CORE endpoint, fix unused variables, and change comment style.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-/* jshint esnext:true */
+// jshint esnext:true
 var _     = require('underscore');
 var koa   = require('koa');
 var route = require('koa-route');
@@ -10,42 +10,49 @@ var chats = wrap(db.get('chats'));
 var trans = wrap(db.get('transcripts'));
 
 var _chats = {
-/* List all chats */
+// List all chats
   list: function* () {
     var res   = yield chats.find({}, {sort:{answeredAt: -1}, limit: 200});
     this.body = res;
   },
 
-/* Grab chats by SSO */
+// Grab chats by SSO
   sso: function* (sso) {
     var _sso  = decodeURI(sso);
     var res   = yield chats.find({rackerSSO: _sso}, {limit: 200});
     this.body = res;
   },
 
-/* Pull one chat by chatID */
+// Pull one chat by chatID
   id: function* (chatID) {
     var _chatID = decodeURI(chatID);
     var res     = yield chats.findOne({chatID: _chatID});
     this.body   = res;
   },
 
-/* Pull chats by customer DDI */
+// Pull chats by customer DDI
   ddi: function* (ddi) {
     var _ddi  = decodeURI(ddi);
-    var res   = yield chats.find({cloudAccount:ddi}, {sort:{answeredAt: -1}});
+    var res   = yield chats.find({cloudAccount:_ddi}, {sort:{answeredAt: -1}});
+    this.body = res;
+  },
+
+// Pull chats by CORE account
+  core: function* (core) {
+    var _core  = decodeURI(core);
+    var res   = yield chats.find({coreAccount:_core}, {sort:{answeredAt: -1}});
     this.body = res;
   }
 };
 
-/* Pull all transcripts for a chat */
+// Pull all transcripts for a chat
 function* _transcripts(chatID) {
   var _chatID = decodeURI(chatID);
   var res     = yield trans.find({chatID: _chatID},{sort:{createdAt: 1}});
   this.body   = res;
 }
 
-/* Averages for EOCR and chat counts */
+// Averages for EOCR and chat counts
 function* _metrics(sso) {
   var _sso        = decodeURI(sso);
   var obj         = {};
@@ -55,7 +62,7 @@ function* _metrics(sso) {
   this.body       = obj;
 }
 
-/* Averages helper function */
+// Averages helper function
 function averages(array) {
   var eocr  = [];
   var arr   = _.filter(array, function(item) { return !!item.chatRating;});
@@ -66,11 +73,12 @@ function averages(array) {
   return ((sum/eocr.length).toFixed(2))/1;
 }
 
-/* Routes */
+// Routes
 app.use(route.get('/api/chats', _chats.list));
 app.use(route.get('/api/chats/:chatid', _chats.id));
 app.use(route.get('/api/chats/sso/:sso', _chats.sso));
 app.use(route.get('/api/chats/ddi/:ddi', _chats.ddi));
+app.use(route.get('/api/chats/core/:core', _chats.core));
 app.use(route.get('/api/metrics/:sso', _metrics));
 app.use(route.get('/api/transcripts/:chatid', _transcripts));
 
